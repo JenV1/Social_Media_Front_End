1b91a430b3df372d6dceddeff69fb161a8c11433
Carol Li <ziqili33@gmail.com>
2022-06-20 12:10:40 +0100

    footer and header done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Footer.css';
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+import './Footer.css';
   
   const Footer = () => {
     return (
-      <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <div id='Footer' className="fixed-bottom">
+      <MDBFooter className="font-small pt-4 mt-4">
         <MDBContainer fluid className="text-center text-md-left">
           <MDBRow>
             <MDBCol md="6">
-              <h5 className="title">Footer Content</h5>
-              <p>
-                Here you can use rows and columns here to organize your footer
-                content.
+              <h5 className='footer-text' >Connect </h5>
+              <p id = "footer-intro">
+                Meet new and interesting friends nearby
               </p>
             </MDBCol>
-            <MDBCol md="6">
-              <h5 className="title">Links</h5>
+            <MDBCol md="6" className='footer-text'>
+              <h5 className="title">Team Members</h5>
               <ul>
                 <li className="list-unstyled">
-                  <a href="#!">Link 1</a>
+                  <a href="https://github.com/JenV1">Jenna Vlahos</a>
                 </li>
                 <li className="list-unstyled">
-                  <a href="#!">Link 2</a>
+                  <a href="https://github.com/Ziqiongg">Ziqiong Li</a>
                 </li>
                 <li className="list-unstyled">
-                  <a href="#!">Link 3</a>
+                  <a href="https://github.com/katyagr">Katya Grenier </a>
                 </li>
                 <li className="list-unstyled">
-                  <a href="#!">Link 4</a>
+                  <a href="https://github.com/JStillman1">James Stillman </a>
                 </li>
               </ul>
             </MDBCol>
@@ -36,10 +40,11 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
         </MDBContainer>
         <div className="footer-copyright text-center py-3">
           <MDBContainer fluid>
-            &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
+            &copy; {new Date().getFullYear()} Copyright: <a href="https://reactjs.org/"> React </a>
           </MDBContainer>
         </div>
       </MDBFooter>
+      </div>
     );
   }
   
