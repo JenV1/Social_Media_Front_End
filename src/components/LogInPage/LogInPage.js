e1bd35e7a2400aa177be7160f50400b0b3667f1f
James Stillman <jamesstillman99@gmail.com>
2022-06-20 14:59:18 +0100

    Finished log in logic check

--- src/components/LogInPage/LogInPage.js
+++ src/components/LogInPage/LogInPage.js
@@ -46,6 +46,10 @@ const LogInPage = () => {
     event.preventDefault()
     const username_input = document.querySelector("#username-input")
     const password_input = document.querySelector("#password-input");
+    const username_error_message = document.querySelector("#username-error-message");
+    const password_error_message = document.querySelector("#password-error-message");
+    username_error_message.innerText = ""
+    password_error_message.innerText = "Password is incorrect"
     username_input.classList.remove("wrong")
     password_input.classList.add("wrong")
   }
@@ -53,6 +57,8 @@ const LogInPage = () => {
   const noSuchUsername = (event) => {
     event.preventDefault();
     const username_input = document.querySelector("#username-input")
+    const username_error_message = document.querySelector("#username-error-message");
+    username_error_message.innerText = "Username does not exist"
     username_input.classList.add("wrong")
   }
 
@@ -69,10 +75,12 @@ const LogInPage = () => {
     <div className="LogInPage">
         <h1>Log In Page</h1>
         <form onSubmit={handleLogInSubmit} action="/">
-          <label htmlFor='username-input'>Username: </label>
+          <label id="username-input-label" htmlFor='username-input'>Username: </label>
           <input onChange={event => setUsername(event.target.value)} value={username} type="text" id="username-input" placeholder='Username/email....' name="username-input"/>
-          <label htmlFor="password-input">Password: </label>
+          <p id="username-error-message"></p>
+          <label id="password-input-label" htmlFor="password-input">Password: </label>
           <input onChange={event => setPassword(event.target.value)} type="password" id="password-input" name="password-input" value={password}/>
+          <p id="password-error-message"></p>
           <input type="submit" id="submit-input"/>
         </form>
         <a href="#">Or Regsiter a new account here instead</a>
