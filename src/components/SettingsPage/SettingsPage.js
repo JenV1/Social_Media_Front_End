595082630596178e8cd3064059a3d08bad3e631a
Katya Grenier <katyagrenier@hotmail.co.uk>
2022-06-22 12:31:39 +0100

    minor edits to properties

--- src/components/SettingsPage/SettingsPage.js
+++ src/components/SettingsPage/SettingsPage.js
@@ -73,7 +73,7 @@ const SettingsPage = () => {
       {showUpdateFields ?  <UpdateForm user={user} /> : null}
       <div className="SettingsButtonContainer">
           {showUpdateFields ? <button onClick={handleClickReturnToSettings}>Return to Settings</button> : null}  
-          {showUpdateDetailsButton ? <UpdateDetails handleClick={handleClickUpdateDetails} /> : null}  
+          {showUpdateDetailsButton ? <UpdateDetails handleClick={handleClickUpdateDetails} /> : null} 
           <a href='/feedPage'><button>Return to Feed</button></a>
       </div>
     </div>
